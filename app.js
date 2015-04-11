16202b93e6a384be2708d05ac2f0b615be890d40
rainyjune <rainyjune@live.cn>
2015-04-11 17:44:14 +0800

    Load AMap API asy

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
-window.onload = initAll;
-function initAll() {
-  var position = new AMap.LngLat(116.397428, 39.90923);
-  var mapObj = new AMap.Map("container", {
-    view: new AMap.View2D({
-      center: position,
-      zoom: 14,
-      rotation: 0
-    }),
-    lang: "zh_cn"
-  });
-  mapObj.plugin(["AMap.ToolBar"],function(){		
-    var toolBar = new AMap.ToolBar();
-    mapObj.addControl(toolBar);		
-  });
-}
+(function(factory){
+  if (typeof define !== "undefined" && define.cmd) {
+    define(function(require, exports, module){
+      var $ = require('zepto');
+      factory($);
+    });
+  } else {
+    var $ = Zepto;
+    factory($);
+  }
+}(function($){
+  
+  loadScript();
+  
+  window.initMap = function () {
+    var mapObj = new AMap.Map("container", {
+      view: new AMap.View2D({
+        zoom: 4
+      })
+    });
+    mapObj.plugin(["AMap.ToolBar"],function(){
+      var toolBar = new AMap.ToolBar();
+      mapObj.addControl(toolBar);		
+    });
+  };
+
+  function loadScript() {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = "http://webapi.amap.com/maps?v=1.3&key=b1fc6abe324303d61dbccbe745be09de&callback=initMap";
+    document.body.appendChild(script);
+  }
+
+}));
+
