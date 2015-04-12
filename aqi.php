c95687e4a884dbac48cce61e0943b5b91beb0435
rainyjune <rainyjune@live.cn>
2015-04-12 12:07:07 +0800

    Update aqi.php: Add cache support.

--- aqi.php
+++ aqi.php
@@ -1,6 +1,30 @@
 <?php
 $cname = $_GET["cname"];
 $c = $_GET["c"];
+if ( !$cname || !$c) {
+  die(json_encode(array(
+    "status" => 0,
+    "info" => "Parameter missing"
+  )));
+}
+
 $url = "http://webmap2.map.bdimg.com/?qt=aqi&cname=".$cname."&c=".$c;
-//var_dump($url);exit;
-echo file_get_contents($url);
+$cacheLife = 3600; // Caching time, in seconds.
+$cnameMd5 = md5($cname);
+$cacheDir = "./data/cache/";
+$cacheFile = $cacheDir.$cnameMd5.".json";
+$filemtime = @filemtime($cacheFile);
+if (file_exists($cacheFile) && (time() - $filemtime) < $cacheLife) {
+  //echo "From cache:\n";
+  echo file_get_contents($cacheFile);
+} else {
+  //echo "From remote:\n";
+  $data = file_get_contents($url);
+  $writeResult = file_put_contents($cacheFile, $data);
+  if ($writeResult === FALSE) {
+    die("Could not write into the file:".$cacheFile);
+  } else {
+    //echo "Write ok:\n";
+    echo $data;
+  }
+}
