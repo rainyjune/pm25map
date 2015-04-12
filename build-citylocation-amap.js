b80c321b0e85ad88bb68a6539d7b7791d53511a4
rainyjune <rainyjune@live.cn>
2015-04-12 10:41:16 +0800

    Update build-citylocation-amap files

--- build-citylocation-amap.js
+++ build-citylocation-amap.js
@@ -1,38 +1,43 @@
 window.onload = initAll;
 function initAll() {
-  var citylistLocation = {};
-  var f = 0;
   
-  AMap.service(["AMap.Geocoder"], function() {
-    var geocoder = new AMap.Geocoder();
-    for(var i = 0; i < citylist.length; i++) {
-      (function(i){
-        var thisCity = citylist[i];
-        geocoder.getLocation(thisCity, function(status, result){
-          f++;
-          if(status === 'complete' && result.info === 'OK'){
-            var thisLocation = result.geocodes[0].location;
-            citylistLocation[thisCity] = {
-              "point": {
-                "lat": thisLocation.lat * 100000,
-                "lng": thisLocation.lng * 100000
-              }
-            };
-          } else {
-            console.warn("Error:", thisCity, i);
-            citylistLocation[thisCity] = {
-              "point": {
-                "lat": 0,
-                "lng": 0
-              }
-            };
-          }
-          if (f == citylist.length) {
-            console.log(JSON.stringify(citylistLocation));
-          }
-        });
-      })(i);
-    }
+  $.getJSON("citylist.json", buildCityLocation);
+  
+  function buildCityLocation(citylist) {
+    var citylistLocation = {};
+    var f = 0;
     
-  });
+    AMap.service(["AMap.Geocoder"], function() {
+      var geocoder = new AMap.Geocoder();
+      for(var i = 0; i < citylist.length; i++) {
+        (function(i){
+          var thisCity = citylist[i];
+          geocoder.getLocation(thisCity, function(status, result){
+            f++;
+            if(status === 'complete' && result.info === 'OK'){
+              var thisLocation = result.geocodes[0].location;
+              citylistLocation[thisCity] = {
+                "point": {
+                  "y": thisLocation.lat * 100000,
+                  "x": thisLocation.lng * 100000
+                }
+              };
+            } else {
+              console.warn("Error:", thisCity, i);
+              citylistLocation[thisCity] = {
+                "point": {
+                  "y": 0,
+                  "x": 0
+                }
+              };
+            }
+            if (f == citylist.length) {
+              console.log(JSON.stringify(citylistLocation));
+            }
+          });
+        })(i);
+      }
+      
+    });
+  }
 }
